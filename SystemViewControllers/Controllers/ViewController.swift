c642cbe6effe9a338240ea9fffb28509aca8dd44
Some name <ronny.coder @gmail.com>
2017-12-10 00:04:30 +0200

    add code to do something with a picked image

--- SystemViewControllers/Controllers/ViewController.swift
+++ SystemViewControllers/Controllers/ViewController.swift
@@ -18,6 +18,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    // UIImagePickerControllerDelegate method
+    // tells app what to do with a picked image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = selectedImage
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
     
     @IBAction func shareButtonAction(_ sender: UIButton) {
         guard let image = imageView.image else { return }

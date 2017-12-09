55345a9da6c9fd61106701561b0e44348d731d6d
Some name <ronny.coder @gmail.com>
2017-12-09 22:12:08 +0200

    starting layout

---
+++ SystemViewControllers/Controllers/BorderedButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderedButton.swift
+//  SystemViewControllers
+//
+//  Created by ronny abraham on 12/9/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class BorderedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

bb05364cc31b36fc69f3e0f82f7357ecef7b45c3
Some name <ronny.coder @gmail.com>
2017-12-09 23:25:36 +0200

    get rid of didcommitMemory

--- SystemViewControllers/Controllers/ViewController.swift
+++ SystemViewControllers/Controllers/ViewController.swift
@@ -17,11 +17,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func shareButtonAction(_ sender: UIButton) {
         guard let image = imageView.image else { return }
